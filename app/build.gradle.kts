de58d3040b886666606f2fd1a1af12685727b8a8
rashid <rashid060803@mail.ru>
2024-06-27 17:52:11 +0300

    1Step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("java")
+    checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
 
 repositories {
     mavenCentral()
@@ -16,4 +18,18 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
